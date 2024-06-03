b834c7bc9aabd03b1ccbd18a19b51f6c8c598ca0
EldarDanilovich <ildar.nasrov@gmail.com>
2024-06-03 08:21:09 +0500

    prepare backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+// import fs from "node:fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const VITE_API_URL = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: "/src",
+      },
+    },
+    server: {
+      // host: true,
+      port: 3000,
+      proxy: {
+        "/api": VITE_API_URL,
+      },
+    },
+  };
+});
