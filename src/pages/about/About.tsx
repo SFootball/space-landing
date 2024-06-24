66c6d4fc31677a6ec1e5d2ba37dc3b28d87e2080
AliKhan <baybutov95@mail.ru>
2024-06-24 05:18:11 +0500

    make 'are you coach?' block

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Container, Text, Divider, Image } from "@chakra-ui/react";
+import { Flex, Box, Container, Text, Divider } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { PreloadImage } from "src/shared/components/PreloadImage";
 import { imgPathMap } from "src/shared/constants/imgPathMap";
@@ -92,199 +92,206 @@ export const About = () => {
   ];
 
   return (
-    <Box as="main">
-      <Box bgColor="gray.100">
-        <Container maxW="1200px">
-          <Flex
-            maxH="2500px"
-            py={{ base: "90px", lg: "140px" }}
-            direction="column"
-            gap="50px"
-          >
+    <>
+      <Box as="main">
+        <Box bgColor="gray.100">
+          <Container maxW="1200px">
             <Flex
-              direction={{ base: "column", lg: "row" }}
-              justifyContent="center"
-              alignItems="center"
+              maxH="2500px"
+              py={{ base: "90px", lg: "140px" }}
+              direction="column"
               gap="50px"
             >
-              <PreloadImage
-                imgSRC={stadium.path}
-                width="300px"
-                height="300px"
-                hash={stadium.hash}
-                borderRadius="50%"
-                objectPosition=""
-              />
-              <Box maxW="600px">
-                <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
-                  {t("Who we are?")}
-                </Text>
-                <Text fontSize={{ base: "14px", lg: "20px" }}>
-                  {t(
-                    "We are the best place to present you as a football player. Join us and kickstart your journey towards football success, regardless of your country. Just follow our promts. And we will take care of the rest."
-                  )}
-                </Text>
-              </Box>
-            </Flex>
-            <Flex
-              direction={{ base: "column-reverse", lg: "row" }}
-              justifyContent="center"
-              alignItems="center"
-              gap="50px"
-            >
-              <Box maxW="600px">
-                <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
-                  {t("Our Mission")}
-                </Text>
-                <Text fontSize={{ base: "14px", lg: "20px" }}>
-                  {t(
-                    "We aim to help young, aspiring football players from all of the world to excel in their professional careers by using contemporary IT solutions, such as machine learning, blockchain, neural networks, customized and user-friendly software. And of course, on the other hand we aim to facilitate the work of coaches, analysts, scouts as well as journalists and commentators."
-                  )}
-                </Text>
-              </Box>
-              <PreloadImage
-                imgSRC={ball.path}
-                width="300px"
-                height="300px"
-                hash={ball.hash}
-                borderRadius="50%"
-                objectPosition="right 0 bottom 0"
-              />
-            </Flex>
-            <Flex
-              direction={{ base: "column", lg: "row" }}
-              justifyContent="center"
-              alignItems="center"
-              gap="50px"
-            >
-              <PreloadImage
-                imgSRC={match.path}
-                width="300px"
-                height="300px"
-                hash={match.hash}
-                borderRadius="50%"
-                objectPosition="left -60px bottom 0"
-              />
-              <Box maxW="600px">
-                <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
-                  {t("How does it work?")}
-                </Text>
-                <Text fontSize={{ base: "14px", lg: "20px" }}>
-                  {t(
-                    "If you are a football player, you just have to sign up on this application and enter your data, such as your date of birth, gender, skills, video of your matches  and so on. After signing up your visibility will be increasing within the football community."
-                  )}{" "}
-                  <Text as="b">
-                    {t("And by the way you will get your own NFT.")}
-                  </Text>
-                </Text>
-              </Box>
-            </Flex>
-            <Flex
-              direction={{ base: "column-reverse", lg: "row" }}
-              justifyContent="center"
-              alignItems="center"
-              gap="50px"
-            >
-              <Box maxW="600px">
-                <Text fontSize={{ base: "14px", lg: "20px" }}>
+              <Flex
+                direction={{ base: "column", lg: "row" }}
+                justifyContent="center"
+                alignItems="center"
+                gap="50px"
+              >
+                <PreloadImage
+                  imgSRC={stadium.path}
+                  width="300px"
+                  height="300px"
+                  hash={stadium.hash}
+                  borderRadius="50%"
+                  objectPosition=""
+                />
+                <Box maxW="600px">
                   <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
-                    {t("If you are not a player")}
+                    {t("Who we are?")}
                   </Text>
-                  ,{" "}
-                  {t(
-                    "you need to sign up as a scout or a user. As a result you will receive all information and tools to closely observe the players."
-                  )}
-                </Text>
-              </Box>
-              <PreloadImage
-                imgSRC={scaut.path}
-                width="300px"
-                height="300px"
-                hash={scaut.hash}
-                borderRadius="50%"
-                objectPosition="left -16px bottom 0"
-              />
+                  <Text fontSize={{ base: "14px", lg: "20px" }}>
+                    {t(
+                      "We are the best place to present you as a football player. Join us and kickstart your journey towards football success, regardless of your country. Just follow our promts. And we will take care of the rest."
+                    )}
+                  </Text>
+                </Box>
+              </Flex>
+              <Flex
+                direction={{ base: "column-reverse", lg: "row" }}
+                justifyContent="center"
+                alignItems="center"
+                gap="50px"
+              >
+                <Box maxW="600px">
+                  <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
+                    {t("Our Mission")}
+                  </Text>
+                  <Text fontSize={{ base: "14px", lg: "20px" }}>
+                    {t(
+                      "We aim to help young, aspiring football players from all of the world to excel in their professional careers by using contemporary IT solutions, such as machine learning, blockchain, neural networks, customized and user-friendly software. And of course, on the other hand we aim to facilitate the work of coaches, analysts, scouts as well as journalists and commentators."
+                    )}
+                  </Text>
+                </Box>
+                <PreloadImage
+                  imgSRC={ball.path}
+                  width="300px"
+                  height="300px"
+                  hash={ball.hash}
+                  borderRadius="50%"
+                  objectPosition="right 0 bottom 0"
+                />
+              </Flex>
+              <Flex
+                direction={{ base: "column", lg: "row" }}
+                justifyContent="center"
+                alignItems="center"
+                gap="50px"
+              >
+                <PreloadImage
+                  imgSRC={match.path}
+                  width="300px"
+                  height="300px"
+                  hash={match.hash}
+                  borderRadius="50%"
+                  objectPosition="left -60px bottom 0"
+                />
+                <Box maxW="600px">
+                  <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
+                    {t("How does it work?")}
+                  </Text>
+                  <Text fontSize={{ base: "14px", lg: "20px" }}>
+                    {t(
+                      "If you are a football player, you just have to sign up on this application and enter your data, such as your date of birth, gender, skills, video of your matches  and so on. After signing up your visibility will be increasing within the football community."
+                    )}{" "}
+                    <Text as="b">
+                      {t("And by the way you will get your own NFT.")}
+                    </Text>
+                  </Text>
+                </Box>
+              </Flex>
+              <Flex
+                direction={{ base: "column-reverse", lg: "row" }}
+                justifyContent="center"
+                alignItems="center"
+                gap="50px"
+              >
+                <Box maxW="600px">
+                  <Text fontSize={{ base: "14px", lg: "20px" }}>
+                    <Text fontSize={{ base: "32px", lg: "46px" }} as="b">
+                      {t("If you are not a player")}
+                    </Text>
+                    ,{" "}
+                    {t(
+                      "you need to sign up as a scout or a user. As a result you will receive all information and tools to closely observe the players."
+                    )}
+                  </Text>
+                </Box>
+                <PreloadImage
+                  imgSRC={scaut.path}
+                  width="300px"
+                  height="300px"
+                  hash={scaut.hash}
+                  borderRadius="50%"
+                  objectPosition="left -16px bottom 0"
+                />
+              </Flex>
             </Flex>
-          </Flex>
-        </Container>
-      </Box>
-      <Box bgColor="white">
-        <Container maxW="1000px">
-          <Box maxH="1600px" pt={{ base: "90px", lg: "140px" }}>
-            <Box mb={{ base: "90px", lg: "140px" }}>
-              <Text fontSize={{ base: "21px", lg: "30px" }} as="b">
-                {t("Management")}
-              </Text>
-              <Divider borderColor="black" mb={{ base: "45px", lg: "90px" }} />
-              <Flex justify="center" align="start">
-                {management.map((person) => (
-                  <Flex
-                    key={person.name}
-                    direction="column"
-                    align="center"
-                    width={{ base: "50%", lg: "25%" }}
-                  >
-                    <PreloadImage
-                      imgSRC={person.imgSRC}
-                      width="120px"
-                      height="120px"
-                      hash={person.hash}
-                      borderRadius="50%"
-                      objectPosition={person.objectPosition}
-                      mb="10px"
-                    />
-                    <Text fontSize={{ base: "13px", lg: "18px" }} as="b">
-                      {t(`${person.name}`)}
-                    </Text>
-                    <Text
-                      textAlign="center"
-                      fontSize={{ base: "13px", lg: "18px" }}
+          </Container>
+        </Box>
+        <Box bgColor="white">
+          <Container maxW="1000px">
+            <Box maxH="1600px" pt={{ base: "90px", lg: "140px" }}>
+              <Box mb={{ base: "90px", lg: "140px" }}>
+                <Text fontSize={{ base: "21px", lg: "30px" }} as="b">
+                  {t("Management")}
+                </Text>
+                <Divider
+                  borderColor="black"
+                  mb={{ base: "45px", lg: "90px" }}
+                />
+                <Flex justify="center" align="start">
+                  {management.map((person) => (
+                    <Flex
+                      key={person.name}
+                      direction="column"
+                      align="center"
+                      width={{ base: "50%", lg: "25%" }}
                     >
-                      {t(`${person.position}`)}
-                    </Text>
-                  </Flex>
-                ))}
-              </Flex>
-            </Box>
-            <Box mb={{ base: "90px", lg: "140px" }}>
-              <Text fontSize={{ base: "21px", lg: "30px" }} as="b">
-                {t("Core team")}
-              </Text>
-              <Divider borderColor="black" mb={{ base: "45px", lg: "90px" }} />
-              <Flex justify="start" align="start" wrap="wrap" rowGap="60px">
-                {coreTeam.map((person) => (
-                  <Flex
-                    key={person.name}
-                    direction="column"
-                    align="center"
-                    width={{ base: "50%", lg: "25%" }}
-                  >
-                    <PreloadImage
-                      imgSRC={person.imgSRC}
-                      width="120px"
-                      height="120px"
-                      hash={person.hash}
-                      borderRadius="50%"
-                      objectPosition={person.objectPosition}
-                      mb="10px"
-                    />
-                    <Text fontSize={{ base: "13px", lg: "18px" }} as="b">
-                      {t(`${person.name}`)}
-                    </Text>
-                    <Text
-                      textAlign="center"
-                      fontSize={{ base: "13px", lg: "18px" }}
+                      <PreloadImage
+                        imgSRC={person.imgSRC}
+                        width="120px"
+                        height="120px"
+                        hash={person.hash}
+                        borderRadius="50%"
+                        objectPosition={person.objectPosition}
+                        mb="10px"
+                      />
+                      <Text fontSize={{ base: "13px", lg: "18px" }} as="b">
+                        {t(`${person.name}`)}
+                      </Text>
+                      <Text
+                        textAlign="center"
+                        fontSize={{ base: "13px", lg: "18px" }}
+                      >
+                        {t(`${person.position}`)}
+                      </Text>
+                    </Flex>
+                  ))}
+                </Flex>
+              </Box>
+              <Box mb={{ base: "90px", lg: "140px" }}>
+                <Text fontSize={{ base: "21px", lg: "30px" }} as="b">
+                  {t("Core team")}
+                </Text>
+                <Divider
+                  borderColor="black"
+                  mb={{ base: "45px", lg: "90px" }}
+                />
+                <Flex justify="start" align="start" wrap="wrap" rowGap="60px">
+                  {coreTeam.map((person) => (
+                    <Flex
+                      key={person.name}
+                      direction="column"
+                      align="center"
+                      width={{ base: "50%", lg: "25%" }}
                     >
-                      {t(`${person.position}`)}
-                    </Text>
-                  </Flex>
-                ))}
-              </Flex>
+                      <PreloadImage
+                        imgSRC={person.imgSRC}
+                        width="120px"
+                        height="120px"
+                        hash={person.hash}
+                        borderRadius="50%"
+                        objectPosition={person.objectPosition}
+                        mb="10px"
+                      />
+                      <Text fontSize={{ base: "13px", lg: "18px" }} as="b">
+                        {t(`${person.name}`)}
+                      </Text>
+                      <Text
+                        textAlign="center"
+                        fontSize={{ base: "13px", lg: "18px" }}
+                      >
+                        {t(`${person.position}`)}
+                      </Text>
+                    </Flex>
+                  ))}
+                </Flex>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-      </Box>
-      <Box bgColor="gray.100">
+          </Container>
+        </Box>
+        {/* <Box bgColor="gray.100">
         <Container maxW="1000px">
           <Box maxH="1200px" py={{ base: "90px", lg: "140px" }}>
             <Text fontSize={{ base: "21px", lg: "30px" }} as="b">
@@ -331,7 +338,9 @@ export const About = () => {
             </Flex>
           </Box>
         </Container>
+      </Box> */}
       </Box>
-    </Box>
+      <Divider />
+    </>
   );
 };
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -4,11 +4,8 @@ import { useTranslation } from "react-i18next";
 
 const videoWithStatImgSRC = "/images/common/are-you-footballer.png";
 const reachForStarImgSRC = "/images/common/reach-for-star.png";
-// const laptopImgSRC_2 = "/images/common/2.png";
-const laptopImgSRC_3 = "/images/common/3.png";
+const areYouCoachImgSRC = "/images/common/are-you-coach.png";
 const laptopImgSRC_4 = "/images/common/4.png";
-// const dataImgSRC_1 = "/images/common/5-1.png";
-// const dataImgSRC_2 = "/images/common/5-2.png";
 
 export const Main = () => {
   const { t } = useTranslation();
@@ -78,11 +75,7 @@ export const Main = () => {
           >
             {t("Who we are?")}
           </Text>
-          <Text
-            display="inline-block"
-            fontSize={{ base: "18px", lg: "26px" }}
-            as="b"
-          >
+          <Text display="block" fontSize={{ base: "18px", lg: "26px" }} as="b">
             {t("FIND OUT ABOUT THE BEST YOUNG FOOTBALL PLAYERS")}
           </Text>
           <Text fontSize={{ base: "14px", lg: "20px" }} mb="54px">
@@ -139,7 +132,7 @@ export const Main = () => {
               <Image
                 borderRadius="30px"
                 src={videoWithStatImgSRC}
-                maxW="48%"
+                maxW={{ base: "90%", md: "48%" }}
                 shadow="inner"
                 p="2"
                 mr={{ base: "0", md: "20px" }}
@@ -188,87 +181,56 @@ export const Main = () => {
               </Box>
               <Image
                 src={reachForStarImgSRC}
-                maxW="48%"
+                maxW={{ base: "90%", md: "48%" }}
                 shadow="inner"
                 p="2"
                 borderRadius="30px"
                 mb={{ base: "20px", md: "0" }}
               />
             </Flex>
-            {/* <Flex justify="space-between" align="center"> 
-              <Box>
-                <Text fontSize={{ base: "18px", lg: "26px" }} as="b" mb="24px">
-                  {t("SFOOTBALL PLATFORM FOR COACHES")}
-                </Text>
-                <Box fontSize={{ base: "13px", lg: "18px" }}>
-                  <p>• {t("Video breakdown of own and opponent’s matches")};</p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Various options to review a match: full match video, ball-in-play mode, and highlights"
-                    )}
-                    ;
-                  </p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Several video tracks available for a review: TV streams or technical footage"
-                    )}
-                    ;
-                  </p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Video player with the option to accelerate the video speed"
-                    )}
-                    ;
-                  </p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Quick video download with the 1080p or 720p resolution"
-                    )}
-                    ;
-                  </p>
-                  <p>• {t("Customizable statistical spreadsheets")}.</p>
-                </Box>
-              </Box> 
-              <Image src={laptopImgSRC_2} maxW="48%" />
-              </Flex>*/}
             <Flex
               justify="space-between"
               align={{ base: "center", md: "start" }}
               direction={{ base: "column", md: "row" }}
             >
               <Image
-                src={laptopImgSRC_3}
-                maxW="48%"
+                src={areYouCoachImgSRC}
+                maxW={{ base: "90%", md: "48%" }}
                 mb={{ base: "20px", md: "0" }}
+                shadow="inner"
+                p="2"
+                borderRadius="30px"
               />
               <Box pl={{ base: "0", md: "100px" }}>
                 <Text fontSize={{ base: "18px", lg: "26px" }} as="b" mb="24px">
-                  {t("SFOOTBALL PLATFORM FOR PLAYER RECRUITMENT")}
+                  {t("ARE YOU A COACH? THEN DON'T PASS BY")}
                 </Text>
                 <Box fontSize={{ base: "13px", lg: "18px" }}>
                   <p>
                     •{" "}
                     {t(
-                      "Filter players by their statistics to find the best candidates"
-                    )}
-                    ;
-                  </p>
-                  <p>• {t("Quick access to review all player actions")};</p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Watch statistical metrics that are linked to the video"
+                      "Get extended match statistics for your team and the opposing team"
                     )}
                     ;
                   </p>
                   <p>
                     •{" "}
                     {t(
-                      "Share any video in two clicks with your team via email"
+                      "Analyze team and individual actions of football players"
+                    )}
+                    ;
+                  </p>
+                  <p>
+                    •{" "}
+                    {t(
+                      "Do you want to achieve new heights in your coaching career? Look for a promising team around the world"
+                    )}
+                    ;
+                  </p>
+                  <p>
+                    •{" "}
+                    {t(
+                      "Do you dream of forming your own team? Look for talented guys around the world"
                     )}
                     .
                   </p>
@@ -294,62 +256,10 @@ export const Main = () => {
               </Box>
               <Image
                 src={laptopImgSRC_4}
-                maxW="48%"
+                maxW={{ base: "90%", md: "48%" }}
                 mb={{ base: "20px", md: "0" }}
               />
             </Flex>
-            {/* <Flex justify="space-between" align="center">
-              <Image src={dataImgSRC_1} maxW="48%" />
-              <Box>
-                <Text fontSize={{ base: "18px", lg: "26px" }} as="b" mb="24px">
-                  {t("PLAYER FITNESS TRACKING REPORTS")}
-                </Text>
-                <Box fontSize={{ base: "13px", lg: "18px" }}>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Fitness tracking data about your own team as well as the opponents"
-                    )}
-                    ;
-                  </p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Speed and covered distance metrics available for each player"
-                    )}
-                    ;
-                  </p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Player position charts showing the team gaps in offense and defense"
-                    )}
-                    ;
-                  </p>
-                  <p>• {t("New metrics combining event and fitness data")};</p>
-                  <p>• {t("Proven data collection methodology")}.</p>
-                </Box>
-              </Box>
-            </Flex>
-            <Flex justify="space-between" align="center">
-              <Box pr="50px">
-                <Text fontSize={{ base: "18px", lg: "26px" }} as="b" mb="24px">
-                  {t("RAW DATA")}
-                </Text>
-                <Box fontSize={{ base: "13px", lg: "18px" }}>
-                  <p>• {t("Over 2500 metrics")};</p>
-                  <p>• {t("Data available in XML and JSON formats")};</p>
-                  <p>
-                    •{" "}
-                    {t(
-                      "Tailor-made assistance and guidance on how to use the data for a website or on social media"
-                    )}
-                    .
-                  </p>
-                </Box>
-              </Box>
-              <Image src={dataImgSRC_2} maxW="48%" />
-            </Flex> */}
           </Flex>
         </Container>
       </Box>
